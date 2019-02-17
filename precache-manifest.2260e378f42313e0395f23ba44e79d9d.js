25c4017f346d97b86fa4227f747ba809743fb46e
deduskaa <jennny.tran@gmail.com>
2019-02-17 15:37:05 +0200

    Updates

---
+++ precache-manifest.2260e378f42313e0395f23ba44e79d9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31227d2d24e70457d8d8",
+    "url": "/movix/static/js/runtime~main.31227d2d.js"
+  },
+  {
+    "revision": "bd88846d86e18a42e977",
+    "url": "/movix/static/js/main.bd88846d.chunk.js"
+  },
+  {
+    "revision": "8b4ae3513ce7d63d73a1",
+    "url": "/movix/static/js/2.8b4ae351.chunk.js"
+  },
+  {
+    "revision": "bd88846d86e18a42e977",
+    "url": "/movix/static/css/main.37021979.chunk.css"
+  },
+  {
+    "revision": "9fab895c4862e1d398ceacae224051e2",
+    "url": "/movix/index.html"
+  }
+];
